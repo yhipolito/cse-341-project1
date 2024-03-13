527c4334608cd966c3a2ee70278850f2139a695b
yhipolito <yazelhipolito@gmail.com>
2024-03-13 00:45:10 -0600

    connected mongodb with VSC

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 const router = require('express').Router();
 
-router.get('/', (req, res) => {res.send('Hello world');});
+// router.get('/', (req, res) => {res.send('Hello Yazel');});
+
+router.use('/users', require('./user.js'));
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,9 +1,21 @@
 const express = require("express");
+
+const mongodb = require("./data/database");
 const app = express();
-const router = express.Router();
+// const router = express.Router();
 
 const port = process.env.PORT || 3000;
 
-app.use('/', router);
+app.use("/", require("./routes"));
 
-app.listen(port, () => {console.log(`Running on port: ${port}`)});
+// app.listen(port, () => {console.log(`Running on port: ${port}`)});
+
+mongodb.initDb((err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    app.listen(port, () => {
+      console.log(`Database is listening and node running on port ${port}`);
+    });
+  }
+});
