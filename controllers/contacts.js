b86289a338dcab3db29bfb1b8704eef7a7fdb316
Yazel Hipolito <yazelhipolito@gmail.com>
2026-07-25 18:48:12 -0600

    update contacts.js

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -2,19 +2,19 @@ const mongodb = require('../data/database');
 const ObjectId = require('mongodb').ObjectId;
 
 const getAll = async (req, res) => {
-    const result = await mongodb.getDatabase().db().collection('users').find();
-    result.toArray().then((users) => {
+    const result = await mongodb.getDatabase().db().collection('contacts').find();
+    result.toArray().then((contacts) => {
         res.setHeader('Content-type', 'application/json');
-        res.status(200).json(users);
+        res.status(200).json(contacts);
     });
 };
 
 const getSingle = async (req, res) => {
     const userId = new ObjectId(req.params.id);
-    const result = await mongodb.getDatabase().db().collection('users').find({_id: userId});
-    result.toArray().then((users) => {
+    const result = await mongodb.getDatabase().db().collection('contacts').find({_id: contactId});
+    result.toArray().then((contacts) => {
         res.setHeader('Content-type', 'application/json');
-        res.status(200).json(users[0]);
+        res.status(200).json(contacts[0]);
     });
 };
 
